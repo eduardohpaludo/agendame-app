cb07f3c38cf7838d8caafeac58846f8d23dea4b3
eduardohpaludo <eduardopaludo@gmail.com>
2023-11-08 07:14:55 -0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <VContainer>
+    <VBtn @click="sanctum">Sanctum</VBtn>
+    <VBtn @click="login">Login</VBtn>
+  </VContainer>
+</template>
+
+<script setup>
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+  function sanctum(){
+    axios.get('http://localhost:8000/sanctum/csrf-cookie').then(r => {
+      console.log(r)
+    })
+  }
+
+  function login(){
+    axios.post('http://localhost:8000/login', {
+      email: 'test@example.com',
+      password: 'password'
+    }).then(r => {
+      console.log(r)
+    })
+  }
+</script>
+
+
