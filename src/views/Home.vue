e255aaf32c73c4289abb93d054d3dc1a2d64e039
eduardohpaludo <eduardopaludo@gmail.com>
2023-11-09 22:48:24 -0300

    Fix login and created exception

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,19 @@
   <VContainer>
     <VBtn @click="sanctum">Sanctum</VBtn>
     <VBtn @click="login">Login</VBtn>
+    <VBtn @click="getUser">User</VBtn>
   </VContainer>
 </template>
 
 <script setup>
+import Cookie from 'js-cookie';
 import axios from 'axios';
 axios.defaults.withCredentials = true;
+
+axios.interceptors.request.use(function (config) {
+    config.headers['X-XSRF-TOKEN'] = Cookie.get('XSRF-TOKEN');
+    return config
+  });
   function sanctum(){
     axios.get('http://localhost:8000/sanctum/csrf-cookie').then(r => {
       console.log(r)
@@ -19,6 +26,12 @@ axios.defaults.withCredentials = true;
       email: 'test@example.com',
       password: 'password'
     }).then(r => {
+      console.log(r)
+    })
+  }
+
+  function getUser(){
+    axios.get('http://localhost:8000/api/user').then(r => {
       console.log(r)
     })
   }
